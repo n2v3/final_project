0098de7c1bb81e5d6165a440a3d735c24bb1273c
Natalia Zhdanova <natalia.zhdanova@NV-MacBook-Pro.local>
2024-01-25 23:33:04 +0200

    Change test_candidate.py

--- job_hub/tests/models/test_candidate.py
+++ job_hub/tests/models/test_candidate.py
@@ -1,27 +1,40 @@
 from django.test import TestCase
-from job_hub.models import Candidate, Location, CompanyProfile, Skill
+from job_hub.models import Candidate, Skill
 
-class EmployerModelTest(TestCase):
+class CandidateModelTest(TestCase):
     def setUp(self):
-
         # Create a Skill instance for testing
         self.skill = Skill.objects.create(skill_name='Test Skill')
 
-    def test_candidate_creation(self):
-        candidate = Candidate.objects.create(
+        # Create a Candidate instance for testing
+        self.candidate = Candidate.objects.create(
             candidate_first_name='Test candidate first name',
             candidate_last_name='Test candidate last name',
             email='Test Email',
         )
 
         # Add the Skill to the many-to-many relationship using set()
-        candidate.skills.set([self.skill])
+        self.candidate.skills.set([self.skill])
 
+    def test_candidate_creation(self):
         # Check if the object was created successfully
-        self.assertIsInstance(candidate, Candidate)
+        self.assertIsInstance(self.candidate, Candidate)
 
         # Additional assertions
-        self.assertEqual(candidate.candidate_first_name, 'Test candidate first name')
-        self.assertEqual(candidate.candidate_last_name, 'Test candidate last name')
-        self.assertEqual(candidate.email, 'Test Email')
-        self.assertEqual(list(candidate.skills.all()), [self.skill])
+        self.assertEqual(self.candidate.candidate_first_name, 'Test candidate first name')
+        self.assertEqual(self.candidate.candidate_last_name, 'Test candidate last name')
+        self.assertEqual(self.candidate.email, 'Test Email')
+        self.assertEqual(list(self.candidate.skills.all()), [self.skill])
+
+    def test_get_skills_list(self):
+        # Retrieve the skills list using the get_skills_list method
+        skills_list = self.candidate.get_skills_list()
+
+        # Check if the skills list contains the expected skills
+        self.assertEqual(len(skills_list), 1)
+        self.assertIn(self.skill, skills_list)
+
+    def test_str_representation(self):
+        # Test the __str__ method for the Candidate model
+        expected_str = 'Test candidate first name Test candidate last name'
+        self.assertEqual(str(self.candidate), expected_str)
