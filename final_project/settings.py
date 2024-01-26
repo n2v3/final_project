3b567280245c968e5093a9ff1f8f88f8167aeb0c
Natalia Zhdanova <natalia.zhdanova@NV-MacBook-Pro.local>
2024-01-26 21:28:22 +0200

    Move secret key to .env

--- final_project/settings.py
+++ final_project/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-70pbisstu5wi%noq=9re*pvwqmz*up)e_"
-              "f-63t2dvbo4fbep!w")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
